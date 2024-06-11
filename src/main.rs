75894ed62fc0ba1218673629419066a8d461edbf
backwardspy <backwardspy@pigeon.life>
2024-06-11 14:28:44 +0100

    feat: support separate bot hook

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,38 @@
-use std::{env, sync::Arc};
+use std::sync::Arc;
 
-use anyhow::Context;
-use axum::{routing::post, Router};
+use axum::{
+    extract::{FromRef, State},
+    routing::post,
+    Router,
+};
 use axum_github_webhook_extract::{GithubEvent, GithubToken};
 use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer};
 use tracing::{error, info, Level};
+
+#[derive(serde::Deserialize)]
+struct Config {
+    github_token: String,
+    discord_webhook: String,
+    discord_bot_webhook: String,
+}
+
+#[derive(Clone)]
+struct DiscordHooks {
+    normal: String,
+    bot: String,
+}
+
+#[derive(Clone)]
+struct AppState {
+    discord_hooks: DiscordHooks,
+    github_token: GithubToken,
+}
+
+impl FromRef<AppState> for GithubToken {
+    fn from_ref(state: &AppState) -> Self {
+        state.github_token.clone()
+    }
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -12,20 +40,22 @@ async fn main() -> anyhow::Result<()> {
         .with_max_level(Level::INFO)
         .init();
 
-    let token = env::var("GITHUB_TOKEN").context("failed to get GITHUB_TOKEN")?;
-    let discord_webhook = env::var("DISCORD_WEBHOOK").context("failed to get DISCORD_WEBHOOK")?;
+    let config: Config = envy::from_env()?;
 
     let app = Router::new()
-        .route(
-            "/webhook",
-            post(move |e| webhook(e, discord_webhook.clone())),
-        )
+        .route("/webhook", post(webhook))
         .layer(
             TraceLayer::new_for_http()
                 .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
                 .on_response(DefaultOnResponse::new().level(Level::INFO)),
         )
-        .with_state(GithubToken(Arc::new(token)));
+        .with_state(AppState {
+            discord_hooks: DiscordHooks {
+                normal: config.discord_webhook,
+                bot: config.discord_bot_webhook,
+            },
+            github_token: GithubToken(Arc::new(config.github_token)),
+        });
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
     let addr = listener.local_addr()?;
@@ -39,37 +69,50 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-async fn webhook(GithubEvent(e): GithubEvent<serde_json::Value>, discord_webhook: String) {
-    if !should_forward(&e) {
-        return;
-    }
+enum HookTarget {
+    Normal,
+    Bot,
+    None,
+}
 
-    match reqwest::Client::new()
-        .post(discord_webhook)
-        .json(&e)
-        .send()
-        .await
-    {
-        Err(e) => error!(?e, "failed to send discord webhook"),
-        Ok(r) => info!(?r, "discord webhook sent"),
+async fn webhook(
+    State(app_state): State<AppState>,
+    GithubEvent(e): GithubEvent<serde_json::Value>,
+) {
+    match hook_target(&e) {
+        HookTarget::Normal => {
+            info!("sending normal hook");
+            send_hook(&e, &app_state.discord_hooks.normal).await;
+        }
+        HookTarget::Bot => {
+            info!("sending bot hook");
+            send_hook(&e, &app_state.discord_hooks.bot).await;
+        }
+        HookTarget::None => info!("ignoring event"),
+    }
+}
+
+async fn send_hook(e: &serde_json::Value, hook: &str) {
+    match reqwest::Client::new().post(hook).json(e).send().await {
+        Err(e) => error!(?e, "failed to send hook"),
+        Ok(r) => info!(?r, "hook sent"),
     };
 }
 
-fn should_forward(e: &serde_json::Value) -> bool {
+fn hook_target(e: &serde_json::Value) -> HookTarget {
     let sender_type = e.pointer("/payload/sender/type").and_then(|v| v.as_str());
     let private = e
         .pointer("/payload/repository/private")
         .and_then(|v| v.as_bool());
 
     if sender_type == Some("Bot") {
-        info!("ignoring bot event");
-        return false;
+        return HookTarget::Bot;
     }
 
     if private == Some(true) {
         info!("ignoring private repository event");
-        return false;
+        return HookTarget::None;
     }
 
-    true
+    HookTarget::Normal
 }
