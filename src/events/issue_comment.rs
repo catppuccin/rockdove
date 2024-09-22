f5c3936593f9a79426ac490c73531d9f13eac91c
sgoudham <sgoudham@gmail.com>
2024-09-22 04:53:53 +0100

    tests: reorganise `issue_comment`

--- src/events/issue_comment.rs
+++ src/events/issue_comment.rs
@@ -52,32 +52,24 @@ mod tests {
         make_embed,
         tests::{embed_context, TestConfig},
     };
+    use std::fs;
+    use yare::parameterized;
 
-    #[test]
-    fn created() {
-        let payload = include_str!("../../fixtures/issue_comment/created.json");
+    #[parameterized(
+        created = { "created" },
+        created_on_pull_request = { "created_on_pull_request" },
+      )]
+    fn snapshot(event_type: &str) {
+        let event = "issue_comment";
+        let root = env!("CARGO_MANIFEST_DIR");
+        let filename = format!("{root}/fixtures/{event}/{event_type}.json");
+        let payload = fs::read_to_string(&filename).expect("fixture exists");
         let TestConfig {
-            event,
+            webhook_event,
             mut settings,
-        } = TestConfig::new("issue_comment", payload);
+        } = TestConfig::new(event, &payload);
 
-        let embed = make_embed(event)
-            .expect("make_embed should succeed")
-            .expect("event fixture can be turned into an embed");
-
-        settings.set_info(&embed_context(&embed));
-        settings.bind(|| insta::assert_yaml_snapshot!(embed));
-    }
-
-    #[test]
-    fn created_on_pull_request() {
-        let payload = include_str!("../../fixtures/issue_comment/created_on_pull_request.json");
-        let TestConfig {
-            event,
-            mut settings,
-        } = TestConfig::new("issue_comment", payload);
-
-        let embed = make_embed(event)
+        let embed = make_embed(webhook_event)
             .expect("make_embed should succeed")
             .expect("event fixture can be turned into an embed");
 
