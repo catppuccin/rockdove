f49a23a387e793008973e5af68b1ade627a51dad
backwardspy <backwardspy@pigeon.life>
2024-09-24 20:24:02 +0100

    feat: ignore requested reviews on userstyles (#31)
    there is no way i can see to detect if a review was requested automatically as
    part of a codeowners configuration, so we just ignore all reviews requested on
    the userstyles repo instead.

--- src/events/pull_request.rs
+++ src/events/pull_request.rs
@@ -22,7 +22,7 @@ pub fn make_embed(
 
     let mut embed = EmbedBuilder::default();
 
-    let repo_name = repo.full_name.unwrap_or(repo.name);
+    let repo_name = repo.full_name.as_ref().unwrap_or(&repo.name);
 
     embed.title(&format!(
         "[{}] Pull request {}: #{} {}",
@@ -51,6 +51,11 @@ pub fn make_embed(
             PullRequestWebhookEventAction::ReadyForReview => "ready for review".to_string(),
             PullRequestWebhookEventAction::Reopened => "reopened".to_string(),
             PullRequestWebhookEventAction::ReviewRequested => {
+                // userstyles is a monorepo with codeowners so generates a lot of noise in this case
+                if repo.name == "userstyles" {
+                    return Ok(None);
+                }
+
                 let mut reviewer_names = vec![];
                 reviewer_names.extend(
                     specifics
@@ -122,9 +127,22 @@ mod tests {
         opened_by_bot = { "opened_by_bot" },
         closed = { "closed" },
         reopened = { "reopened" },
-        multiple_reviewers = { "multiple_reviewers" }
+        multiple_reviewers = { "multiple_reviewers" },
     )]
     fn snapshot(event_type: &str) {
         snapshot_test!("pull_request", event_type);
+    }
+
+    #[test]
+    fn test_userstyles_is_banned() {
+        let embed = crate::events::make_embed(
+            octocrab::models::webhook_events::WebhookEvent::try_from_header_and_body(
+                "pull_request",
+                include_str!("../../fixtures/pull_request/userstyles_review.json"),
+            )
+            .expect("event fixture is valid"),
+        )
+        .expect("make_embed should succeed");
+        assert_eq!(embed, None, "userstyles should be ignored");
     }
 }
